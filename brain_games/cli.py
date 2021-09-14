18209200428b9e03ec9461be0d9e0da1ccb65e0c
exproots <nonenull11@icloud.com>
2021-09-14 17:29:49 +0300

    project Makefile lint

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 import prompt
+
+
 def welcome_user():
     name = prompt.string('May I have your name? ')
-    print('Hello, '+ name + '!')
+    print('Hello, ' + name + '!')
+
 
 def main():
     welcome_user()
+
 
 if __name__ == '__main__':
     welcome_user()
