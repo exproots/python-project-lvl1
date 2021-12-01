205d8608879ce8064931a7ccf4e00cf871c543f7
exproots <nonenull11@icloud.com>
2021-12-01 14:09:06 +0300

    fix flake8 2

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -2,8 +2,7 @@
 from random import randint
 
 
-TASK_DESCRIPTION = 'Answer "yes" if given number is prime. '\
-    'Otherwise answer "no".'
+TASK_DESCRIPTION = 'Answer "yes" if given number is prime.''Otherwise answer "no".'
 
 first_number = 1
 second_number = 30
--- brain_games/logic_functions.py
+++ brain_games/logic_functions.py
@@ -26,9 +26,7 @@ def get_game(game):
             print('Correct!')
         else:
             if answer_user != correct_answer:
-                print("'" + answer_user +\
-                     "' is wrong answer ;(. Correct answer was '" \
-                         + correct_answer + "'.")
+                print("'" + answer_user +' is wrong answer ;(. Correct answer was ' + correct_answer + "'.")
                 print("Let's try again, " + name + "!")
                 exit()
         step_round += 1
