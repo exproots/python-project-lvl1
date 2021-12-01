d94de6eb7a4af62f3ac2d79202a51a53c96915cc
exproots <nonenull11@icloud.com>
2021-12-01 13:33:02 +0300

    fix 1.0

--- brain_games/games/brain_prime.py
+++ brain_games/games/brain_prime.py
@@ -5,19 +5,19 @@ from random import randint
 TASK_DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 first_number = 1
-second_number = 10
+second_number = 30
 
 
 def check_prime(prime_number):
     """Функция для определения простого числа!"""
+    if prime_number == 1:
+        return False
     k = 0
     for i in range(2, prime_number // 2 + 1):
         if (prime_number % i == 0):
             k += 1
     if (k <= 0):
         return True
-    if prime_number < 2:
-        return False
     else:
         return False
 
