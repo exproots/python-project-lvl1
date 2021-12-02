f01014fbf4e9887de877156ed3555c8067e8c83d
exproots <nonenull11@icloud.com>
2021-12-02 12:05:27 +0300

    fix flake8 3

--- brain_games/logic_functions.py
+++ brain_games/logic_functions.py
@@ -18,15 +18,16 @@ def get_game(game):
     while step_round <= count_rounds:
         question, correct_answer = game.get_game_round()
         print(f'Question: {question}')
-        answer_user = prompt.string('You answer: ')
-        if step_round == count_rounds and answer_user == correct_answer:
+        answer_usr = prompt.string('You answer: ')
+        if step_round == count_rounds and answer_usr == correct_answer:
             print('Correct!')
             print('Congratulations, ' + name + '!')
-        if answer_user == correct_answer and step_round != count_rounds:
+        if answer_usr == correct_answer and step_round != count_rounds:
             print('Correct!')
         else:
-            if answer_user != correct_answer:
-                print("'" + answer_user + ' is wrong answer ;(. Correct answer was ' + correct_answer + "'.")
+            if answer_usr != correct_answer:
+                print("'" + answer_usr + ' is wrong answer ;(.'
+                      'Correct answer was ' + correct_answer + "'.")
                 print("Let's try again, " + name + "!")
                 exit()
         step_round += 1
